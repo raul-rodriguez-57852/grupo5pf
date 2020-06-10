d9b4f96bf39bc7368b5203e590c5d1e6ba9d42f7
Naim <naimsaadi7@gmail.com>
2020-06-10 20:57:25 -0300

    EDU-25
    Creado backend clase tutor

---
+++ Implementacion/EnDesarrollo/apirest/src/main/java/com/edusis/apirest/domain/Tutor.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.edusis.apirest.domain;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author Naim Saadi
+ */
+@Entity
+@DiscriminatorValue(value=Persona.DTYPE_TUTOR)
+public class Tutor extends Persona {
+
+}
---
+++ Implementacion/EnDesarrollo/apirest/src/main/java/com/edusis/apirest/service/TutorService.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.edusis.apirest.service;
+
+import com.edusis.apirest.domain.Tutor;
+
+/**
+ *
+ * @author Naim Saadi
+ */
+
+public interface TutorService extends PersonaService<Tutor> {
+
+}
